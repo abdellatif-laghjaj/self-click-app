422a69497c50abb983223ee3a1c9a150665d0616
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-07 01:26:27 +0100

    fix some bugs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject AdManager;
 
     // Update is called once per frame
     void Update()
